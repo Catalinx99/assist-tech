270962cc95b7e3838dc17ddfb12d3893e7d16283
CosminaX <cosminaalupoaei@yahoo.com>
2022-03-03 18:25:49 +0200

    fixing conflits

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,9 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Switch, } from 'react-router-dom';
 import Header from '../Header/Header';
-
 import ForgotPsw from "../Login/ForgotPsw";
 import LogIn from "../Login/LogIn";
+import UsersManagement from '../Users Management Page/UsersManagement';
 
 function App() {
   return (
@@ -13,6 +13,7 @@ function App() {
         <Switch>
           <Route exact path="/" component={LogIn} />
           <Route path="/forgotpw" component={ForgotPsw} />
+          <Route path="/usermanagement" component={UsersManagement} />
         </Switch>
       </div>
     </Router>
---
+++ src/components/Building Management/BuildingManagement.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const BuildingManagement = () => {
+  return (
+    <div>
+    </div>
+  )
+}
+
+export default BuildingManagement
