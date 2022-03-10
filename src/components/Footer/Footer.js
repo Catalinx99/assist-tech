5dce332ff28b7472383dccb6aad8ea1b6328a482
CosminaX <cosminaalupoaei@yahoo.com>
2022-03-10 15:59:35 +0200

    Footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,16 @@
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footers">
+      <p className="copyright">
+        &copy; Assist Tech
+      </p>
+      <p className="teamName">
+        In IT to win IT
+      </p>
+    </div>
+  )
+}
+
+export default Footer
