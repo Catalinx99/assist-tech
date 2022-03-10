6412d71329ad5d7f991f2b2c4c293e92cf250404
Prisacaru Catalin <80458624+Catalinx99@users.noreply.github.com>
2022-03-10 16:08:42 +0200

    Revert "Login page (#18)" (#20)
    This reverts commit 451f1026afc6eed4e6423f32e1760004c5aaa5b0.

--- src/components/Login/ForgotPsw.js
+++ src/components/Login/ForgotPsw.js
@@ -1,21 +1,21 @@
 import "./LoginCss.css"
 
 const ForgotPsw = () => {
-  return (
-    <div className="center">
-      <a href="/" className="close"> &times; </a>
-      <h1>Forgot password</h1>
-      <form method="post">
-        <div className="txt_field">
-          {/* TO DO change the text type into email */}
-          <input type="text" required />
-          <span></span>
-          <label> Email </label>
-        </div>
-        <input type="submit" value="Submit" />
-      </form>
+return (
+<div className="center">
+  <a href="/" className="close"> &times; </a>
+  <h1>Forgot password</h1>
+  <form method="post">
+    <div className="txt_field">
+      {/* TO DO change the text type into email */}
+      <input type="text" required />
+      <span></span>
+      <label> Email </label>
     </div>
-  )
+    <input type="submit" value="Submit" />
+  </form>
+</div>
+)
 }
 
 export default ForgotPsw
