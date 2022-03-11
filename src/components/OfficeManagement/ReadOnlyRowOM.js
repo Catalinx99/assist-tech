b8b86f191e076d49b64e9cbd83e0897ddefecd16
Prisacaru Catalin <p.catalinv@yahoo.com>
2022-03-11 16:29:47 +0200

    Create Office-Management page

---
+++ src/components/OfficeManagement/ReadOnlyRowOM.js
@@ -0,0 +1,20 @@
+import "../UsersManagementPage/UsersManagement.css"
+
+const ReadOnlyRowOM = ({ office, handleEditClick, handleDeleteClick }) => {
+  return (
+    <tr>
+      <td className='officeField'> {office.officeName} </td>
+      <td className='officeField'> --- </td>
+      <td className='officeField'> {office.floorsNumber} </td>
+      <td className='officeField'> {office.deskCount} </td>
+      <td>
+        <button type='button' onClick={(event) => handleEditClick(event, office)} className="button">
+          Edit
+        </button>
+        <button type="button" className="button" onClick={() => handleDeleteClick(office.id)}> Delete </button>
+      </td>
+    </tr >
+  )
+}
+
+export default ReadOnlyRowOM
