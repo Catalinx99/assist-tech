5af8bf70ef5438218c302fd4f15fc007839a3cca
Prisacaru Catalin <p.catalinv@yahoo.com>
2022-03-13 20:58:49 +0200

    Fake login with json server

--- src/components/Login/LogIn.js
+++ src/components/Login/LogIn.js
@@ -1,25 +1,71 @@
 import "./LoginCss.css"
-
+import { useState } from "react";
+import serviceApi from "../services"
 const LogIn = () => {
+	const services = new serviceApi();
+	const login = (data) => {
+		services.get(`users/?email=${data.email}&password=${data.password}`).then(res => {
+			if (res && res.length > 0) {
+				const formattedUserData = res[0];
+				delete formattedUserData.password;
+				localStorage.setItem('user', JSON.stringify(formattedUserData));
+				switch (formattedUserData.role) {
+					case 'user':
+						window.location.replace('/office-status');
+						break;
+					case 'administrator':
+						window.location.replace('/user-management');
+						break;
+					case 'officeAdministrator':
+						window.location.replace('/office-status');
+						break;
+
+					default:
+						break;
+				}
+			}
+			else {
+				localStorage.clear();
+			}
+		})
+
+	}
+
+	const [loginFormData, setLoginFormData] = useState({
+		email: '',
+		password: '',
+	})
+
+	const handleAddFormChange = (event) => {
+		event.preventDefault();
+		const fieldName = event.target.name;
+		const fieldValue = event.target.value;
+
+		const newFormData = { ...loginFormData };
+		newFormData[fieldName] = fieldValue;
+
+		setLoginFormData(newFormData);
+	};
+
 	return (
 		<div>
 			<div className="center">
 				<h1>Login</h1>
-				<form method="post">
+				<form>
 					<div className="txt_field">
-						<input type="text" required />
+						<input type="text" name="email" required onChange={handleAddFormChange} />
 						<span></span>
-						<label> Username </label>
+						<label> Email </label>
 					</div>
 					<div className="txt_field">
-						<input type="password" required />
+						<input type="password" name="password" required onChange={handleAddFormChange} />
 						<span></span>
 						<label> Password </label>
 					</div>
 					<div className="pass">
 						<a href="/forgotpw"> Forgot password ?</a>
 					</div>
-					<input type="submit" value="Login" />
+					<button type="button" className="loginButton" onClick={() => login(loginFormData)}>Login</button>
 				</form>
 			</div>
 		</div>
