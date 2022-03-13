4f8c1cd53792618d923f9ba3f6d043bb72cd4622
CosminaX <99120230+CosminaX@users.noreply.github.com>
2022-03-13 15:42:19 +0200

    Buildings issues (#29)
    * Accept changes
    
    * Delete button

--- src/components/BuildingManagement/ReadOnlyRowBM.js
+++ src/components/BuildingManagement/ReadOnlyRowBM.js
@@ -1,17 +1,37 @@
 /* eslint-disable eqeqeq */
 import "../UsersManagementPage/UsersManagement.css";
 
-const ReadOnlyRowBM = ({ building, handleEditClick, handleDeleteClick }) => {
+const ReadOnlyRowBM = ({
+  building,
+  handleEditClick,
+  handleDeleteClick,
+  offices
+}) => {
+  const filteredOffices = offices.filter((office) => {
+    return office.buildingId === building.id
+  })
+  const officeIsEmpty = filteredOffices.filter((item) => {
+    return item.occupiedDesksCount !== 0
+  })
   return (
     <tr>
       <td className='buildingField'> {building.name} </td>
       <td className='buildingField'> {building.floorsCount} </td>
       <td className='buildingField'> {building.address} </td>
       <td>
-        <button type='button' onClick={(event) => handleEditClick(event, building)} className="button">
+        <button
+          type='button'
+          onClick={(event) => handleEditClick(event, building)}
+          className="button"
+        >
           Edit
         </button>
-        <button type='button' onClick={() => handleDeleteClick(building.id)} className="button">
+        <button
+          type='button'
+          onClick={() => officeIsEmpty.length === 0 && handleDeleteClick(building.id)}
+          className="button"
+          disabled={officeIsEmpty.length > 0}
+        >
           Delete
         </button>
       </td>
