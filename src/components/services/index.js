869e950ee2903c0e079a9395af29eb5a7cf5c3e7
Prisacaru Catalin <p.catalinv@yahoo.com>
2022-03-17 13:25:35 +0200

    server port log

--- src/components/services/index.js
+++ src/components/services/index.js
@@ -4,6 +4,7 @@ const headers = {
   'Content-Type': 'application/json'
 }
 // const domain = 'http://localhost:3001';
+// const domain = 'http://localhost:4000';
 const domain = 'https://assist-tech.herokuapp.com:4000';
 
 function joinURL(baseURL, url) {
